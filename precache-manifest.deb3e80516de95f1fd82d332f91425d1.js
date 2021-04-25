fe95533a602a5f4942292524d5a90663185878d3
Pei Chiang <sctessa@gmail.com>
2021-04-25 22:51:08 +0800

    Updates

---
+++ precache-manifest.deb3e80516de95f1fd82d332f91425d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7226b9c81fddae92d89b089cb565b55c",
+    "url": "/ReactShop/index.html"
+  },
+  {
+    "revision": "719eaa3fe98213858a07",
+    "url": "/ReactShop/static/css/main.2b646858.chunk.css"
+  },
+  {
+    "revision": "05e6c2fb3cd2a1e5cdea",
+    "url": "/ReactShop/static/js/2.90cc0fb8.chunk.js"
+  },
+  {
+    "revision": "719eaa3fe98213858a07",
+    "url": "/ReactShop/static/js/main.e85fda22.chunk.js"
+  },
+  {
+    "revision": "adb77f4977fb5a076e45",
+    "url": "/ReactShop/static/js/runtime~main.a9101baf.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/ReactShop/static/media/crown.19ef7b17.svg"
+  }
+]);
