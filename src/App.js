f8cb1da8a4989bc042917bbf4ba4ad7a706e7d19
Pei Chiang <sctessa@gmail.com>
2021-04-26 00:47:20 +0800

    modified directory linkurl

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
       <div>
         <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route path='/' component={HomePage} />
+          <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import MenuItem from '../menu-item/menu-item.component';
-
 import './directory.styles.scss';
 
 class Directory extends React.Component {
@@ -14,33 +12,33 @@ class Directory extends React.Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'hats'
+          linkUrl: './hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: ''
+          linkUrl: './jackets'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: ''
+          linkUrl: './jackets'
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: ''
+          linkUrl: './womens'
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
           size: 'large',
           id: 5,
-          linkUrl: ''
+          linkUrl: './mens'
         }
       ]
     };
