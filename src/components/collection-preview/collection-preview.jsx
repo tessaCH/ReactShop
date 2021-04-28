42f603fd0fad9003daa4d9dbec5bfe1190902eda
tessaCH <sctessa@gmail.com>
2021-04-28 17:53:37 +0800

    add cart actions

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
@@ -10,9 +9,14 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
+        .map(({ item }) => (
+          <CollectionItem key={item.id} item={item} />
+        ))
+        /*items
+        .filter((item, idx) => idx < 4)
         .map(({ id, ...otherItemProps }) => (
           <CollectionItem key={id} {...otherItemProps} />
-        ))}
+        ))*/}
     </div>
   </div>
 );
