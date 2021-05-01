09d3e26b04af70e91c335ef7f706811c1f62dc68
Pei Chiang <sctessa@gmail.com>
2021-05-02 00:10:44 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactShop/precache-manifest.59f282d7210a5d1c77faa15174a67d01.js"
+  "/ReactShop/precache-manifest.bd4bd292860a8ba92725643027d8b211.js"
 );
 
 self.addEventListener('message', (event) => {
